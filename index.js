df48ec6f7f112d7a2a3a55affa00b3363149d9b3
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-09-02 13:25:19 -0500

    started function input

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function getIssues() {
+
 }
 
 function showIssues(json) {
+
 }
 
 function createIssue() {
+  var title = document.getElementById('title').value;
+  var text = document.getElementById('body').value;
+  var issues = document.getElementById('issues');
+  if (title && text) {
+    issues.innerHTML += title;
+    issues.innerHTML += text;
+    console.log("issue added");
+  } else {
+    alert("you need to add both title and description");
+    console.log("issue not added");
+  }
+  fetch('https://api.github.com/Jimmy6strings/fcc-sept-2017',
+  {
+    method: 'POST',
+    body: JSON.stringify(json),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
 }
 
 function showResults(json) {
+
 }
 
 function forkRepo() {
-  const repo = 'itzsaga/fcc-sept-2017'
+  const repo = 'Jimmy6strings/fcc-sept-2017'
   //use fetch to fork it!
+  showIssues();
 }
 
 function getToken() {
